b27ca633b6ca7225b4f842e310b0dbf1577876e6
Sanal Sabu <sanalsabu22@gmail.com>
2025-02-06 18:59:22 +0530

    update for aws ec2

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'whitenoise.runserver_nostatic'
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -70,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
  
 ]
 
@@ -268,3 +270,14 @@ RAZORPAY_KEY_ID = os.getenv('RAZORPAY_KEY_ID')
 RAZORPAY_KEY_SECRET = os.getenv('RAZORPAY_KEY_SECRET')
  
 
+
+#AWS bucket
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -14,21 +14,46 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path,include
+# from . import settings
+# from django.conf.urls.static import static
+ 
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path("",include('store.urls')),
+#     path("store_manager/",include('store_manager.urls')),
+#     path('accounts/', include('allauth.urls')),
+    
+# ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:  # Ensure this is only added in debug mode
+#     import debug_toolbar
+#     urlpatterns = [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ] + urlpatterns
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
- 
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include('store.urls')),
-    path("store_manager/",include('store_manager.urls')),
+    path("", include('store.urls')),
+    path("store_manager/", include('store_manager.urls')),
     path('accounts/', include('allauth.urls')),
-    
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
 
-if settings.DEBUG:  # Ensure this is only added in debug mode
+# Serve media files in development
+if settings.DEBUG:  
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # Corrected STATIC_URL to STATIC_ROOT
+
+# Debug toolbar
+if settings.DEBUG: 
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
